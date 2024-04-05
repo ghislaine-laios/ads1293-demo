e887f7285099842bc0b24f59ea3c12bad9b5942b
Ghislaine Laios <laios54@pm.me>
2024-04-05 12:32:45 +0800

    Refactor the project layout

--- examples/normal/src/main.rs
+++ examples/normal/src/main.rs
@@ -1,14 +1,7 @@
-use ads1293_demo::driver::initialization::Application3Lead;
 use ads1293_demo::driver::initialization::Initializer;
-use ads1293_demo::driver::registers;
-use ads1293_demo::driver::registers::access::ReadFromRegister;
-use ads1293_demo::driver::registers::DataRegister;
-use ads1293_demo::driver::registers::DATA_STATUS;
-use ads1293_demo::driver::ADS1293;
 use embassy_futures::select::select;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::Channel;
-use embedded_hal::spi::SpiDevice;
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 use esp_idf_svc::hal::gpio::OutputPin;
 use esp_idf_svc::hal::gpio::PinDriver;
@@ -21,19 +14,17 @@ use esp_idf_svc::hal::task;
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::timer::EspTaskTimerService;
 use esp_idf_sys::esp;
-use esp_idf_sys::esp_timer_get_time;
 use futures_util::SinkExt;
 use normal::communication::communication;
 use normal::communication::connect_wifi;
 use normal::communication::discover_service;
 use normal::communication::setup_websocket;
 use normal::communication::ConnectWifiPayload;
+use normal::data;
 use normal::settings::Settings;
 use normal_data::Data;
 use std::net::SocketAddr;
 use std::time::Duration;
-
-use tokio::sync::mpsc::error::TrySendError;
 
 static DATA_CHANNEL: embassy_sync::channel::Channel<
     CriticalSectionRawMutex,
@@ -132,7 +123,7 @@ fn main() -> anyhow::Result<()> {
                 socket.flush().await.unwrap();
             };
 
-            select(data(device, tx), ws).await;
+            select(data::data(device, tx), ws).await;
             log::error!("some routine exits!");
         });
 
@@ -156,82 +147,5 @@ async fn led<L: OutputPin>(led_pin: L) {
         next_low = !next_low;
 
         tokio::time::sleep(Duration::from_secs(1)).await;
-    }
-}
-
-async fn data(device: impl SpiDevice, data_sender: tokio::sync::mpsc::Sender<Data>) {
-    let mut ads1293 = ADS1293::new(device);
-
-    ads1293
-        .init(Application3Lead)
-        .expect("failed to init the ads1293");
-
-    log::info!("ADS1293 initialized.");
-
-    let main_config = ads1293
-        .read(registers::CONFIG)
-        .expect("failed to read the main config");
-
-    log::info!("main_config: {:#?}", main_config);
-
-    let loop_back_mode_config = ads1293
-        .read(registers::CH_CNFG)
-        .expect("fail to read loop back mode config");
-    log::info!("loop_back_mode_config: {:#?}", loop_back_mode_config);
-
-    let mut id = 0;
-    let mut perf_counter = 0;
-    let mut fail_counter = 60;
-    let mut start_time = unsafe { esp_timer_get_time() };
-    let frame_duration = Duration::from_secs_f32(1.0 / 60.0);
-    let mut sleep_deadline = tokio::time::Instant::now();
-    'out: loop {
-        tokio::time::sleep_until(sleep_deadline).await;
-        perf_counter += 1;
-        sleep_deadline = sleep_deadline.checked_add(frame_duration).unwrap();
-
-        let _data_status = ads1293.read(DATA_STATUS).expect("fail to read data status");
-
-        let data_vec = ads1293
-            .stream_one()
-            .expect("failed to read data under stream mode");
-        log::trace!("data: {:?}", data_vec);
-
-        for data in data_vec {
-            let DataRegister::DATA_CH1_ECG(data) = data else {
-                continue;
-            };
-
-            if let Err(e) = data_sender.try_send(Data {
-                id,
-                value: data.into(),
-            }) {
-                match e {
-                    // I drop the data directly to ensure that
-                    // the data's ID remains reliable for timing purposes.
-                    TrySendError::Full(_data) => {
-                        if fail_counter == 60 {
-                            log::warn!("the data channel is full. the data will be dropped.");
-                            fail_counter = 0;
-                        }
-
-                        fail_counter += 1;
-                    }
-                    TrySendError::Closed(_) => break 'out,
-                }
-            }
-
-            break;
-        }
-
-        id += 1;
-
-        if perf_counter == 600 {
-            let end_time = unsafe { esp_timer_get_time() };
-            let span = end_time - start_time;
-            start_time = end_time;
-            log::info!("last 600 frame: {} ms", span / 1000);
-            perf_counter = 0;
-        }
     }
 }
